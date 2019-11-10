cdb24d76df02f2360a396151b1f939230d4dff7a
Nikita Karatsev <nikita.karatsev@gmail.com>
2019-11-10 15:24:03 +0600

    [~]lab2

--- second-semester/lab2/1.cpp
+++ second-semester/lab2/1.cpp
@@ -8,138 +8,157 @@
 #include <string.h>
 #include <ctype.h>
 
-
 using namespace std;
 
-	struct A{
-		int key;
-		int birthYear;
-		char firstName[20];
-		char secondName[20];
-		char patronyc[20];
-	}*ptr, *tmpptr;
+struct A
+{
+	int key;
+	char firstName[20];
+	char lastName[20];
+	char lastLastName[20];
+	int birthYear;
 
-void PrintAll(struct A* ptr,struct A* endptr){
-	struct A* tmpptr = ptr;
-		 cout << "Фамилия    Имя      Отчество  Год   Ключ\n";
-		 while (tmpptr != endptr){
-			cout << tmpptr->secondName << " " << tmpptr->firstName << " " << tmpptr->patronyc
-			<< " " << tmpptr->birthYear << "  " << tmpptr->key << "\n";
-			tmpptr++;
-		 }
-	}
+} * ptr, *tmpptr;
 
+void printStruct(struct A *ptr, struct A *endptr);
+int testBirth();
 
-int main(void) {
-	int fd; /* Файловый дескриптор для файла, в котором будет храниться информация*/
+int main(void)
+{
+	int fd;		   /* Файловый дескриптор для файла, в котором будет храниться информация*/
 	size_t length; /* Длина отображаемой части файла */
 	int i;
-	char ch;
+	char menu;
 
 	fd = open("file.txt", O_RDWR | O_CREAT, 0666);
-	if( fd == -1) {
-	/* Если файл открыть не удалось, выдаем сообщение об ошибке и завершаем работу */
+
+	if (fd == -1)
+	{
 		printf("File open failed!\n");
 		exit(1);
 	}
-	length = 100*sizeof(struct A);
-	ftruncate(fd,length);
-	ptr = (struct A*)mmap(NULL, length, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
+
+	length = 2 * sizeof(struct A);
+	ftruncate(fd, length);
+	ptr = (struct A *)mmap(0, length, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
 	close(fd);
-	if ( ptr == MAP_FAILED ){
-/* Если отобразить файл не удалось, сообщаем об ошибке и завершаем работу */
-	printf("Mapping failed!\n");
-	exit(2);
+
+	if (ptr == MAP_FAILED)
+	{
+		printf("Mapping failed!\n");
+		exit(2);
 	}
-	
+
 	tmpptr = ptr;
 
-	while(true){
-		PrintAll(ptr, tmpptr);
+	while (menu != '3')
+	{
+		printStruct(ptr, tmpptr);
 		int flag = 0;
-		cout << "Меню:\n1.Добавление\n2.Изменение поля\n3.Выход.\nВвод: ";
-		cin >> ch;
-		switch(ch) {
-			case '3':
-				flag = 1;
-				break;
-			case '1':
-				cout << "Введите Имя: ";
-				cin >> tmpptr->firstName;
-				cout<< "Введите Фамилию: ";
-				cin >> tmpptr->secondName;
-				cout<< "Введите Отчество: ";
-				cin >> tmpptr->patronyc;
-				cout << "Введите ключ: ";
-				cin >> tmpptr->key;
-				cout << "Введите год рождения: ";
-				cin >> tmpptr->birthYear;
-				tmpptr++;
-				break;
-			case '2': 
-				char symbol, s[100];
-				int k;
-				cout << "Введите ключ записи, которую вы хотите изменить: ";
-				cin >> k;
-				struct A* tmpptr1 = ptr;
-				struct A* keyPtr;
+		int birth = 0;
+		cout << "Меню:"
+			 << "\n\t1.Добавление"
+			 << "\n\t2.Изменение поля"
+			 << "\n\t3.Выход."
+			 << "\nВвод: ";
+		cin >> menu;
+		switch (menu)
+		{
+		case '1':
+			cout << "Введите ключ: ";
+			cin >> tmpptr->key;
+			cout << "Введите Имя: ";
+			cin >> tmpptr->firstName;
+			cout << "Введите Фамилию: ";
+			cin >> tmpptr->lastName;
+			cout << "Введите Отчество: ";
+			cin >> tmpptr->lastLastName;
+			tmpptr->birthYear = testBirth();
+			tmpptr++;
+			break;
+		case '2':
+			char symbol;
+			int k;
+			cout << "Введите ключ записи, которую вы хотите изменить: ";
+			cin >> k;
+			struct A *tmpptr1 = ptr;
+			struct A *keyPtr;
 
-				while (tmpptr1 != tmpptr) {
-					if (tmpptr1->key == k) {keyPtr = tmpptr1; break;}
-					tmpptr1++;				
+			while (tmpptr1 != tmpptr)
+			{
+				if (tmpptr1->key == k)
+				{
+					keyPtr = tmpptr1;
+					break;
 				}
+				tmpptr1++;
+			}
 
-
-				while(true) {
-					int flag1 = 0;
-					cout << "Выберите поля, которые вы хотите изменить:\n1.ФИО\n2.Год рождения\n3.Ключ\n4.Закончить изменение полей\nВвод: ";
-					cin >> symbol;
-					switch(symbol) {
-						case '1':
-							cout << "Введите Имя: ";
-							cin >> keyPtr->firstName;
-							cout<< "Введите Фамилию: ";
-							cin >> keyPtr->secondName;
-							cout<< "Введите Отчество: ";
-							cin >> keyPtr->patronyc;
-							break;
-						case '2':
-							cout << "Введите год рождения: ";
-							cin >> keyPtr->birthYear;
-							break;
-						case '3':
-							cout << "Введите ключ: ";
-							cin >> keyPtr->key;
-							break;
-						case '4':
-							flag1 = 1;
-							break;
-					}
-					if (flag1 == 1){ flag1 = 0; break; } 
+			while (symbol != '4')
+			{
+				cout << "Выберите поля, которые вы хотите изменить:"
+					 << "\n\t1.Ключ"
+					 << "\n\t2.ФИО"
+					 << "\n\t3.Год рождения"
+					 << "\n\t4.Закончить изменение полей"
+					 << "\nВвод: ";
+				cin >> symbol;
+				switch (symbol)
+				{
+				case '1':
+					cout << "Введите ключ: ";
+					cin >> keyPtr->key;
+					break;
+				case '2':
+					cout << "Введите Имя: ";
+					cin >> keyPtr->firstName;
+					cout << "Введите Фамилию: ";
+					cin >> keyPtr->lastName;
+					cout << "Введите Отчество: ";
+					cin >> keyPtr->lastLastName;
+					break;
+				case '3':
+					cout << "Введите год рождения: ";
+					cin >> keyPtr->birthYear;
+					break;
 				}
-				break;
+			}
+			break;
 		}
-		if (flag == 1) {break;}
 	}
-/* Прекращаем отображать файл в память, записываем содержимое отображения на диск и
-освобождаем память. */
+
 	munmap((void *)ptr, length);
 	return 0;
 }
 
-/*void PrintAll(struct A* ptr,struct A* endptr){
-	struct A* tmpptr = ptr;
-		 while (tmpptr != endptr){
-			cout << "Фамилия    Имя      Отчество  Год   Ключ\n" << tmpptr->secondName << " " << tmpptr->firstName << " " << tmpptr->patronyc
-			<< " " << tmpptr->birthYear << "  " << tmpptr->key << "\n";
-			tmpptr++;
-		 }
-	}*/
-/*A* Finding(A* ptr, A* endptr, int key){
-	A* tmpptr = ptr;
-	while (tmpptr != (endptr +1)){
-		if (tmpptr->key == key) {return tmpptr;}
+void printStruct(struct A *ptr, struct A *endptr)
+{
+	struct A *tmpptr = ptr;
+	cout << "Ключ"
+		 << "\t\tФамилия"
+		 << "\t\tИмя"
+		 << "\t\tОтчество"
+		 << "\tГод\n";
+
+	while (tmpptr != endptr)
+	{
+		cout << tmpptr->key
+			 << "\t\t" << tmpptr->lastName
+			 << "\t\t" << tmpptr->firstName
+			 << "\t\t" << tmpptr->lastLastName
+			 << "\t\t" << tmpptr->birthYear
+			 << endl;
+		tmpptr++;
 	}
-	return tmpptr;
-}*/
-	
+}
+
+int testBirth()
+{
+	int birth = 0;
+	while ((birth < 1900) || (birth > 2010))
+	{
+		cout << "Введите год рождения: ";
+		cin >> birth;
+	}
+	return birth;
+}
--- second-semester/lab2/1a.cpp
+++ second-semester/lab2/1a.cpp
@@ -7,126 +7,184 @@
 #include <iostream>
 #include <string.h>
 #include <ctype.h>
+
 using namespace std;
 
-	struct A{
-		int key;
-		int birthYear;
-		char firstName[20];
-		char secondName[20];
-		char patronyc[20];
-	}*ptr, *tmpptr, *superTmpPtr;
+struct A
+{
+	int key;
+	int birthYear;
+	char firstName[20];
+	char lastName[20];
+	char lastLastName[20];
+} * ptr, *tmpptr, *superTmpPtr;
 
-struct A* Finding(struct A* ptr,struct A* endptr, int key){
-	struct A* tmpptr = ptr;
+struct A *Finding(struct A *ptr, struct A *endptr, int key)
+{
+	struct A *tmpptr = ptr;
 
 	return tmpptr;
 }
 
-void PrintAll(struct A* ptr,struct A* endptr){
-	struct A* tmpptr = ptr;
-		 cout << "\n\nФамилия    Имя      Отчество  Год   Ключ\n";
-		 while (tmpptr != endptr){
-			cout << tmpptr->secondName << " " << tmpptr->firstName << " " << tmpptr->patronyc
-			<< " " << tmpptr->birthYear << "  " << tmpptr->key << "\n";
-			tmpptr++;
-		 }
-	}
+void print(struct A *ptr, struct A *endptr);
 
-int main(void) {
-	int fd; /* Файловый дескриптор для файла, в котором будет храниться информация*/
+int main()
+{
+	int fd;		   /* Файловый дескриптор для файла, в котором будет храниться информация*/
 	size_t length; /* Длина отображаемой части файла */
 	int i;
-	char ch, c[100];
-	
-	
-	
+	char menu, c[100];
+	//O_CREAT
+	fd = open("file.txt", O_RDWR, 0666);
 
-	fd = open("file.txt", O_RDWR | O_CREAT, 0666);
-	if( fd == -1) {
-	/* Если файл открыть не удалось, выдаем сообщение об ошибке и завершаем работу */
+	if (fd == -1)
+	{
+		/* Если файл открыть не удалось, выдаем сообщение об ошибке и завершаем работу */
 		printf("File open failed!\n");
 		exit(1);
 	}
-	length = 20*sizeof(struct A);
-	ftruncate(fd,length);
-	ptr = (struct A*)mmap(NULL, length, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
-	close(fd);
-	if ( ptr == MAP_FAILED ){
-/* Если отобразить файл не удалось, сообщаем об ошибке и завершаем работу */
-	printf("Mapping failed!\n");
-	exit(2);
-	}
-	tmpptr = ptr;
-	while (tmpptr->key !=NULL) {tmpptr++;}
-	PrintAll(ptr, tmpptr);
-	cout << "\n\n";
-	while(true){
-		int flag = 0;
-		PrintAll(ptr,tmpptr);
-		cout << "Меню:\n1.Вывод записей с заданным ключом\n2.Вывод записей с одинаковым значением полей\n3.Вывод записей с значением года рождения в заданном диапазоне\n4.Выход.\nВвод: ";
-		cin >> ch;
 
-		if (ch == '4'){
-				flag = 1;
-				break;
-		 } else if (ch == '1'){
-				int k;
-				cout << "Введите ключ записи: ";
-				cin >> k;
-				cout << "Фамилия    Имя      Отчество  Год   Ключ\n";
-				struct A* tmpptr1 = ptr;
-				while (tmpptr1 != tmpptr){
-					if (tmpptr1->key == k) {
-						cout << tmpptr1->secondName << " " << tmpptr1->firstName << " " << tmpptr1->patronyc
-						<< " " << tmpptr1->birthYear << "  " << tmpptr1->key << "\n";
-					}
-					tmpptr1++;
+	length = 2 * sizeof(struct A);
+	ftruncate(fd, length);
+	ptr = (struct A *)mmap(0, length,  PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	close(fd);
+
+	if (ptr == MAP_FAILED)
+	{
+		printf("Mapping failed!\n");
+		exit(2);
+	}
+
+	tmpptr = ptr;
+
+	while (tmpptr->key != NULL)
+	{
+		tmpptr++;
+	}
+
+	print(ptr, tmpptr);
+	cout << "\n\n";
+
+	while (menu != '4')
+	{
+		print(ptr, tmpptr);
+		cout << "Меню:"
+			 << "\n\t1.Вывод записей с заданным ключом"
+			 << "\n\t2.Вывод записей с одинаковым значением полей"
+			 << "\n\t3.Вывод записей с значением года рождения в заданном диапазоне"
+			 << "\n\t4.Выход."
+			 << "\nВвод: ";
+		cin >> menu;
+		switch (menu)
+		{
+		case '1':
+		{
+			int k;
+			cout << "Введите ключ записи: ";
+			cin >> k;
+			cout << "Ключ"
+				 << "\t\tФамилия"
+				 << "\t\tИмя"
+				 << "\t\tОтчество"
+				 << "\tГод\n";
+			struct A *tmpptr1 = ptr;
+			while (tmpptr1 != tmpptr)
+			{
+				if (tmpptr1->key == k)
+				{
+					cout << tmpptr1->lastName << " " << tmpptr1->firstName << " " << tmpptr1->lastLastName
+						 << " " << tmpptr1->birthYear << "  " << tmpptr1->key << "\n";
 				}
-		} else if (ch == '2'){
-				int flag = 0;
-				cout << "Фамилия    Имя      Отчество  Год   Ключ\n";
-				for (struct A* currPtr = ptr; currPtr != tmpptr; currPtr++){
-					for (struct A* superTmpPtr1 = (currPtr+1); superTmpPtr1 < tmpptr; superTmpPtr1++){
-						/*cout << currPtr->key << " "<<superTmpPtr1->key<<"||"<<currPtr->firstName<<" "<<superTmpPtr1->firstName
-						<<"||"<<currPtr->patronyc<<" "<<superTmpPtr1->patronyc
-						<<"||"<<currPtr->birthYear<<" "<<superTmpPtr1->birthYear<<"||"<<currPtr->secondName<<" "<<superTmpPtr1->secondName<<" ||"<<flag <<"\n\n";*/
-						if ( ((currPtr->key) == (superTmpPtr1->key)) && ( strcmp(currPtr->firstName, superTmpPtr1->firstName) == 0 ) 
-						&& ( strcmp(currPtr->patronyc, superTmpPtr1->patronyc) == 0 ) && ((currPtr->birthYear) == (superTmpPtr1->birthYear)) 
-						&& ( strcmp(currPtr->secondName, superTmpPtr1->secondName) == 0 )) {
-							if (flag == 0) {
-								cout<< currPtr->secondName << " " << currPtr->firstName << " " << currPtr->patronyc
-								<< " " << currPtr->birthYear << "  " << currPtr->key << "\n";
-								cout<< superTmpPtr1->secondName << " " << superTmpPtr1->firstName << " " << superTmpPtr1->patronyc
-								<< " " << superTmpPtr1->birthYear << "  " << superTmpPtr1->key << "\n";
-								flag = 1;
-							} else {
-								cout<< superTmpPtr1->secondName << " " << superTmpPtr1->firstName << " " << superTmpPtr1->patronyc
-								<< " " << superTmpPtr1->birthYear << "  " << superTmpPtr1->key << "\n";
-							}
+				tmpptr1++;
+			}
+			break;
+		}
+		case '2':
+		{
+			int flag = 0;
+			cout << "Ключ"
+				 << "\t\tФамилия"
+				 << "\t\tИмя"
+				 << "\t\tОтчество"
+				 << "\tГод\n";
+			for (struct A *currPtr = ptr; currPtr != tmpptr; currPtr++)
+			{
+				for (struct A *superTmpPtr1 = (currPtr + 1); superTmpPtr1 < tmpptr; superTmpPtr1++)
+				{
+					if (((currPtr->key) == (superTmpPtr1->key)) && (strcmp(currPtr->firstName, superTmpPtr1->firstName) == 0) && (strcmp(currPtr->lastLastName, superTmpPtr1->lastLastName) == 0) && ((currPtr->birthYear) == (superTmpPtr1->birthYear)) && (strcmp(currPtr->lastName, superTmpPtr1->lastName) == 0))
+					{
+						if (flag == 0)
+						{
+							cout << currPtr->lastName << " " << currPtr->firstName << " " << currPtr->lastLastName
+								 << " " << currPtr->birthYear << "  " << currPtr->key << "\n";
+							cout << superTmpPtr1->lastName << " " << superTmpPtr1->firstName << " " << superTmpPtr1->lastLastName
+								 << " " << superTmpPtr1->birthYear << "  " << superTmpPtr1->key << "\n";
+							flag = 1;
+						}
+						else
+						{
+							cout << superTmpPtr1->lastName << " " << superTmpPtr1->firstName << " " << superTmpPtr1->lastLastName
+								 << " " << superTmpPtr1->birthYear << "  " << superTmpPtr1->key << "\n";
 						}
 					}
-					flag = 0;
 				}
-		} else if (ch == '3'){
-				int left, right, i=0;
-				cout << "Введите левую границу диапазона: ";
-				cin >> left;
-				cout << "Введите правую границу диапазона: ";
-				cin >> right;
-				superTmpPtr = ptr;
-				cout << "Фамилия    Имя      Отчество  Год   Ключ\n";
-				while (superTmpPtr != tmpptr){
-					if (superTmpPtr->birthYear <= right && superTmpPtr->birthYear >=left) {
-						cout<< superTmpPtr->secondName << " " << superTmpPtr->firstName << " " << superTmpPtr->patronyc
-						<< " " << superTmpPtr->birthYear << "  " << superTmpPtr->key << "\n";
-					}
-					superTmpPtr++;
+				flag = 0;
+			}
+			break;
+		}
+
+		case '3':{
+			int left, right, i = 0;
+			cout << "Введите левую границу диапазона: ";
+			cin >> left;
+			cout << "Введите правую границу диапазона: ";
+			cin >> right;
+			superTmpPtr = ptr;
+			cout << "Ключ"
+				 << "\t\tФамилия"
+				 << "\t\tИмя"
+				 << "\t\tОтчество"
+				 << "\tГод\n";
+			while (superTmpPtr != tmpptr)
+			{
+				if (superTmpPtr->birthYear <= right && superTmpPtr->birthYear >= left)
+				{
+					cout << superTmpPtr->lastName << " " << superTmpPtr->firstName << " " << superTmpPtr->lastLastName
+						 << " " << superTmpPtr->birthYear << "  " << superTmpPtr->key << "\n";
 				}
-		} 
+				superTmpPtr++;
+			}
+			break;
+		}
+
+		case '4':
+			break;
+
+		default:
+			break;
+		}
 	}
-/* Прекращаем отображать файл в память, записываем содержимое отображения на диск и
-освобождаем память. */
 	munmap((void *)ptr, length);
 	return 0;
+}
+
+void print(struct A *ptr, struct A *endptr)
+{
+	struct A *tmpptr = ptr;
+	cout << "Ключ"
+		 << "\t\tФамилия"
+		 << "\t\tИмя"
+		 << "\t\tОтчество"
+		 << "\t\tГод\n";
+
+	while (tmpptr != endptr)
+	{
+		cout << tmpptr->key
+			 << "\t" << tmpptr->lastName
+			 << "\t" << tmpptr->firstName
+			 << "\t" << tmpptr->lastLastName
+			 << "\t" << tmpptr->birthYear
+			 << endl;
+		tmpptr++;
+	}
 }
